441a1d1bb4870bb7df8192d73b2c6025d0585a92
Ashley Gatta <161656406+AshleyGattaPedare@users.noreply.github.com>
2024-07-20 19:23:23 +0930

    Create Task 3.py

---
+++ Task 3.py
@@ -0,0 +1,19 @@
+#The screen on the fridge will need to print out the list of ingredients availabel and suggest a recipe if certain ingredients are detected.
+
+# Step 1: On line 5, set up a list called 'fridge_items' which has at least five different items you would find in a fridge.
+
+
+
+# Step 2: On line 9, use a print statement to show 'Items currently in the fridge." On the following two lines, use a for loop to print all of the items in the fridge.
+
+print
+for 
+
+
+# Step 3: Looking at the items in your list, use an if statement to check if two or three of the items can make a recipe. There's an example below. 
+
+if "bacon" in fridge_items and "eggs" in fridge_items:
+    print("You have bacon and eggs! How about cooking bacon and eggs?")
+
+
+
